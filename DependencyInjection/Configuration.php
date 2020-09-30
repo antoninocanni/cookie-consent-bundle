eef2d89c33188bb35685b56a0a3b2eea3292e686
antoninocanni <67139866+antoninocanni@users.noreply.github.com>
2020-09-30 14:05:02 +0200

    Updated fork

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('cookie_consent');
         $rootNode = $treeBuilder->root(Constant::BUNDLE_CONFIG_ROOT);
 
         // Here you should define the parameters that are allowed to
